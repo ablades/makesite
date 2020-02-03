fba5a24e4f259058f0b9a2f520b597d323e2a327
ablades <audaris@gmail.com>
2020-02-02 17:31:34 -0800

    complete save file function

--- makesite.go
+++ makesite.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"html/template"
 	"io/ioutil"
-	"os"
 )
 
 type post struct {
@@ -11,8 +11,9 @@ type post struct {
 	Content string
 }
 
-func readFile(name string) string {
-	fileContents, err := ioutil.ReadFile(name)
+//Reads in a file and returns contents as string
+func readFile(fileName string) string {
+	fileContents, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		panic(err)
 	}
@@ -20,24 +21,39 @@ func readFile(name string) string {
 	return string(fileContents)
 }
 
-func renderTemplate(content string) *template.Template {
+//Renders the template with given content
+func renderTemplate(content string) string {
 	paths := []string{
 		"template.tmpl",
 	}
+
+	buff := new(bytes.Buffer)
 	//will get the template at filename and store it in t. t can then be executed to show the template.
 	t := template.Must(template.New("template.tmpl").ParseFiles(paths...))
-	err := t.Execute(os.Stdout, post{User: "Audi", Content: content})
+	err := t.Execute(buff, post{User: "Audi", Content: content})
 	if err != nil {
 		panic(err)
 	}
 
-	return t
+	return buff.String()
+}
+
+//Saves rendered template to a file
+func saveFile(template string, fileName string) bool {
+	bytesToWrite := []byte(template)
+	err := ioutil.WriteFile(fileName, bytesToWrite, 0644)
+
+	if err != nil {
+		return false
+	}
+
+	return true
 }
 
 func main() {
-
 	content := readFile("first-post.txt")
 	t := renderTemplate(content)
 	print(t)
+	saveFile(t, "first-post.html")
 
 }
